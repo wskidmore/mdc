eeecb19e45d0f3e12957f804d8a8e09b5716bbc1
William Skidmore <wskidmore@gmail.com>
2012-06-06 01:47:26 -0700

    added proof of concept firebolt via right click

--- Assets/Scripts/Spells/FireBolt.js
+++ Assets/Scripts/Spells/FireBolt.js
@@ -11,18 +11,19 @@ class FireBolt extends Spell {
 		// test for collision
 		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		var hit : RaycastHit;
-		var targetPos : Vector3 = (playerTrans.position + Vector3(0, 0, 15));
+		var targetPos : Vector3 = (playerTrans.position + (playerTrans.forward * 100));
 		if (Physics.Raycast(ray, hit, 100))
 		{
 			targetPos = hit.point;
-			Debug.Log('raycast hit');
 		}
+		Debug.Log(targetPos.x + ', '+ targetPos.y + ', '+ targetPos.z);
 	
+		// create the particles/objects
 		instance = GameObject.Instantiate(prefab, playerTrans.position, playerTrans.rotation) as GameObject;
-
-		Debug.Log(targetPos);
-
-		MoveToPoint(instance.transform, playerTrans.position, targetPos, speed);
+		var instStraightScript : Straight = instance.GetComponent(Straight);
+		instStraightScript.speed = speed;
+		instStraightScript.target = targetPos;
+		instStraightScript.moving = true;
 	}
 	
 }
--- Assets/Scripts/Spells/Spell.js
+++ Assets/Scripts/Spells/Spell.js
@@ -1,31 +1,15 @@
 #pragma strict
 class Spell {
-function Cast(playerTrans : Transform){
-	Debug.Log('casting spell from: '+playerTrans.position.x+', '+playerTrans.position.y);
-}
-
-function CanLearn(player) {
-	return true;
-}
-
-function CanCast(currentMP) {
-	return true;
-}
-
-function MoveToPoint(objTransform : Transform, startPos : Vector3, endPos : Vector3, time : float) {
-	var i = 0.0;
-	var rate = 1.0/time;
-	if (i < 1.0)
-	{
-		i += Time.deltaTime * rate;
-		Debug.Log('moving at rate '+ i + ' from '+startPos + ' to '+endPos);
-		objTransform.position = Vector3.Lerp(startPos, endPos, i);
-		yield WaitForEndOfFrame();
-		Move(objTransform, startPos, endPos, time - Time.deltaTime);
+	function Cast(playerTrans : Transform){
+		Debug.Log('casting spell from: '+playerTrans.position.x+', '+playerTrans.position.y);
+	}
+	
+	function CanLearn(player) {
+		return true;
+	}
+	
+	function CanCast(currentMP) {
+		return true;
 	}
 }
-}
 
-function Move(objTransform : Transform, startPos : Vector3, endPos : Vector3, time : float) {
-	MoveToPoint(objTransform, startPos, endPos, time);
-}
